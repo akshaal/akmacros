4b5fc6299112898de0126bd2ed81fb498c9ba758
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2012-09-04 21:05:57 +0300

    Adopted to 2.9.2

--- project/build.scala
+++ project/build.scala
@@ -7,21 +7,11 @@ import org.ensime.sbt.util.SExp._
 object build extends Build {
     val sharedSettings = Defaults.defaultSettings ++ Seq(
         organization := "info.akshaal",
-        version := "0.2-SNAPSHOT",
-        scalaVersion := "2.10.0-M7",
-        scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
+        version := "0.1",
+        scalaVersion := "2.9.2",
+        scalacOptions ++= Seq("-unchecked", "-deprecation"),
         javacOptions ++= Seq("-Xmx1536m", "-Xms512m", "-Xss10m"),
         javaOptions ++= Seq("-Xmx1536m", "-Xms512m", "-Xss10m"),
-
-        libraryDependencies <<= scalaVersion { scala_version =>
-            Seq(
-                "org.scala-lang" % "scala-reflect" % scala_version % "provided",
-
-                // Test dependencies
-                "org.specs2" %% "specs2" % "1.12.1.1" % "test" cross CrossVersion.full,
-                "org.scalacheck" % "scalacheck" % "1.10.0" % "test" cross CrossVersion.full
-            )
-        },
 
         ensimeConfig := sexp(
             key(":compiler-args"), sexp("-Ywarn-dead-code", "-Ywarn-shadowing"),
