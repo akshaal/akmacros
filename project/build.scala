039edb8aba076ed66dd04b840106de101ee8c8af
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2012-09-10 22:52:27 +0300

    Pre-publish...

--- project/build.scala
+++ project/build.scala
@@ -6,8 +6,12 @@ import org.ensime.sbt.util.SExp._
 
 object build extends Build {
     val sharedSettings = Defaults.defaultSettings ++ Seq(
+        licenses := Seq("Apache 2.0" -> url("http://opensource.org/licenses/apache2.0.php")),
+        homepage := Some(url("http://www.akshaal.info/search/label/macro")),
+
         organization := "info.akshaal",
         version := "0.3-SNAPSHOT",
+
         scalaVersion := "2.10.0-M7",
         scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
         javacOptions ++= Seq("-Xmx1536m", "-Xms512m", "-Xss10m"),
@@ -33,7 +37,34 @@ object build extends Build {
                 key(":preserveDanglingCloseParenthesis"), true,
                 key(":indentSpaces"), 4
             )
-        ))
+        ),
+
+        publishMavenStyle := true,
+
+        publishArtifact in Test := false,
+
+        publishTo <<= version { (v: String) =>
+            val nexus = "https://oss.sonatype.org/"
+            if (v.trim.endsWith("SNAPSHOT"))
+                Some("snapshots" at nexus + "content/repositories/snapshots")
+            else
+                Some("releases" at nexus + "service/local/staging/deploy/maven2")
+        },
+
+        pomExtra := (
+            <scm>
+                <url>git@github.com:akshaal/akmacros.git</url>
+                <connection>scm:git@github.com:akshaal/akmacros.git</connection>
+            </scm>
+            <developers>
+                <developer>
+                    <id>akshaal</id>
+                    <name>Evgeny Chukreev</name>
+                    <url>http://akshaal.info</url>
+                </developer>
+            </developers>
+        )
+    )
 
     lazy val root = Project(
         id = "macros",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 addSbtPlugin ("org.ensime" % "ensime-sbt-cmd" % "0.0.10")
+
+resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases")) (Resolver.ivyStylePatterns)
+  
+addSbtPlugin("com.jsuereth" % "xsbt-gpg-plugin" % "0.6")
